81e7fc3b6bbc9080f78162c58a73c5ff56a0297a
Leeseojune <sung07288346@gmail.com>
2023-05-29 22:45:54 +0900

    Reactor로 Response Type 변경

--- src/main/kotlin/io/github/leeseojune53/springwebflux/domain/user/service/UserService.kt
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/user/service/UserService.kt
@@ -2,16 +2,17 @@ package io.github.leeseojune53.springwebflux.domain.user.service
 
 import io.github.leeseojune53.springwebflux.domain.user.model.Token
 import io.github.leeseojune53.springwebflux.domain.user.repository.UserRepository
+import reactor.core.publisher.Mono
 
 class UserService(
     private val userRepository: UserRepository
 ) {
 
-    fun registerUser(userId: String, password: String): Token {
+    fun registerUser(userId: String, password: String): Mono<Token> {
         TODO()
     }
 
-    fun authUser(userId: String, password: String): Token {
+    fun authUser(userId: String, password: String): Mono<Token> {
         TODO()
     }
 
--- src/main/kotlin/io/github/leeseojune53/springwebflux/domain/vote/service/VoteService.kt
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/vote/service/VoteService.kt
@@ -3,16 +3,18 @@ package io.github.leeseojune53.springwebflux.domain.vote.service
 import io.github.leeseojune53.springwebflux.domain.vote.enum.VoteStatus
 import io.github.leeseojune53.springwebflux.domain.vote.model.VoteElement
 import io.github.leeseojune53.springwebflux.domain.vote.repository.VoteRepository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 class VoteService(
     private val voteRepository: VoteRepository
 ) {
 
-    fun getVoteList(): List<VoteElement> {
+    fun getVoteList(): Flux<VoteElement> {
         TODO()
     }
 
-    fun getVoteStatus(voteId: String): VoteStatus {
+    fun getVoteStatus(voteId: String): Mono<VoteStatus> {
         TODO()
     }
 
