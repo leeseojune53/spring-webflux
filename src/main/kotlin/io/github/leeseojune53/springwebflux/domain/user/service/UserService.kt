63672ff5fb64267c672c7c514ff7e0e5b0632067
Leeseojune <sung07288346@gmail.com>
2023-06-04 23:28:34 +0900

    Service annotation 추가

--- src/main/kotlin/io/github/leeseojune53/springwebflux/domain/user/service/UserService.kt
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/user/service/UserService.kt
@@ -2,8 +2,10 @@ package io.github.leeseojune53.springwebflux.domain.user.service
 
 import io.github.leeseojune53.springwebflux.domain.user.model.Token
 import io.github.leeseojune53.springwebflux.domain.user.repository.UserRepository
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
+@Service
 class UserService(
     private val userRepository: UserRepository
 ) {
--- src/main/kotlin/io/github/leeseojune53/springwebflux/domain/vote/service/VoteService.kt
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/vote/service/VoteService.kt
@@ -3,9 +3,11 @@ package io.github.leeseojune53.springwebflux.domain.vote.service
 import io.github.leeseojune53.springwebflux.domain.vote.enum.VoteStatus
 import io.github.leeseojune53.springwebflux.domain.vote.model.VoteElement
 import io.github.leeseojune53.springwebflux.domain.vote.repository.VoteRepository
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
+@Service
 class VoteService(
     private val voteRepository: VoteRepository
 ) {
