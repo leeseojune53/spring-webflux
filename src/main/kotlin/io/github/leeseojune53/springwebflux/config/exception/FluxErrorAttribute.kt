c58c8e51f4591433bdbf2cfeaccaa34adc5e4b73
Leeseojune <sung07288346@gmail.com>
2023-06-04 16:10:15 +0900

    Flux Exception Handler

--- src/main/kotlin/io/github/leeseojune53/springwebflux/config/exception/FluxErrorAttribute.kt
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/config/exception/FluxErrorAttribute.kt
@@ -2,8 +2,10 @@ package io.github.leeseojune53.springwebflux.config.exception
 
 import org.springframework.boot.web.error.ErrorAttributeOptions
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 
+@Component
 class FluxErrorAttribute: DefaultErrorAttributes() {
 
     override fun getErrorAttributes(request: ServerRequest?, options: ErrorAttributeOptions?): MutableMap<String, Any> {
