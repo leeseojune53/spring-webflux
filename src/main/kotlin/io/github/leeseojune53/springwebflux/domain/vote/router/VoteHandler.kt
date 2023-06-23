1e1ef9b0c53c1caea0dcd83a754fc5c27def5244
Leeseojune <sung07288346@gmail.com>
2023-06-23 09:06:52 +0900

    투표 Router, Handler 추가

---
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/vote/router/VoteHandler.kt
@@ -0,0 +1,28 @@
+package io.github.leeseojune53.springwebflux.domain.vote.router
+
+import io.github.leeseojune53.springwebflux.domain.vote.model.VoteElement
+import io.github.leeseojune53.springwebflux.domain.vote.service.VoteService
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import reactor.core.publisher.Mono
+
+@Component
+class VoteHandler(
+    private val voteService: VoteService
+) {
+
+    fun getVoteList(serverRequest: ServerRequest): Mono<ServerResponse> {
+        val result = voteService.getVoteList()
+        return ok().contentType(MediaType.APPLICATION_JSON).body(result, VoteElement::class.java)
+    }
+
+    fun getVoteStatus(serverRequest: ServerRequest): Mono<ServerResponse> {
+        val voteId = serverRequest.pathVariable("voteId")
+        val result = voteService.getVoteStatus(voteId)
+        return ok().contentType(MediaType.APPLICATION_JSON).body(result, String::class.java)
+    }
+
+}
---
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/vote/router/VoteRouter.kt
@@ -0,0 +1,18 @@
+package io.github.leeseojune53.springwebflux.domain.vote.router
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.router
+
+@Configuration
+class VoteRouter {
+
+    @Bean
+    fun voteBaseRouter(voteHandler: VoteHandler) = router {
+        "/votes".nest {
+            GET("/", voteHandler::getVoteList)
+            GET("/{voteId}/status", voteHandler::getVoteStatus)
+        }
+    }
+
+}
