a64aefad7c628aa214d85d79009e01e265965ce8
Leeseojune <sung07288346@gmail.com>
2023-07-05 23:43:50 +0900

    Add Jwt Filter

---
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/config/security/JwtTokenFilter.kt
@@ -0,0 +1,24 @@
+package io.github.leeseojune53.springwebflux.config.security
+
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+
+class JwtTokenFilter(
+    private val jwtTokenProvider: JwtTokenProvider
+): WebFilter {
+
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        val token = jwtTokenProvider.resolveToken(exchange.request)
+        if (token != null) {
+            val authentication = jwtTokenProvider.authentication(token)
+            return ReactiveSecurityContextHolder.getContext().map { context -> context.authentication = authentication }.then(chain.filter(exchange))
+        }
+
+        return chain.filter(exchange)
+    }
+
+
+}
--- src/main/kotlin/io/github/leeseojune53/springwebflux/config/security/SecurityConfig.kt
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/config/security/SecurityConfig.kt
@@ -3,26 +3,35 @@ package io.github.leeseojune53.springwebflux.config.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
 @EnableWebFluxSecurity
-class SecurityConfig {
+class SecurityConfig(
+    jwtTokenProvider: JwtTokenProvider
+) {
+
+    private val jwtTokenFilter = JwtTokenFilter(jwtTokenProvider)
 
     @Bean
     fun getPasswordEncoder() = BCryptPasswordEncoder()
 
     @Bean
-    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain = http
-        .csrf().disable()
-        .formLogin().disable()
-        .httpBasic().disable()
-        .authorizeExchange()
-        .pathMatchers("/users/**").permitAll()
-        .pathMatchers("/votes/**").permitAll()
-        .anyExchange().authenticated()
-        .and()
-        .build()
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http.authorizeExchange { exchange ->
+            exchange.pathMatchers("/users/**").permitAll()
+            exchange.pathMatchers("/votes/**").permitAll()
+            exchange.anyExchange().authenticated()
+        }
+            .csrf().disable()
+            .formLogin().disable()
+            .httpBasic().disable()
+
+        http.addFilterAt(jwtTokenFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+
+        return http.build()
+    }
 }
