0ea37d3e61b3ce23f88266a5e90bec1cbe73ba98
Leeseojune <sung07288346@gmail.com>
2023-07-05 23:43:37 +0900

    Resolve Token

--- src/main/kotlin/io/github/leeseojune53/springwebflux/config/security/JwtTokenProvider.kt
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/config/security/JwtTokenProvider.kt
@@ -3,10 +3,39 @@ package io.github.leeseojune53.springwebflux.config.security
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import org.springframework.http.server.reactive.ServerHttpRequest
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.User
 import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
 
 @Component
 class JwtTokenProvider {
+
+    companion object {
+        private val TOKEN_HEADER = "Authorization"
+        private val TOKEN_PREFIX = "Bearer"
+        private val SECRET_KEY = "asdkfj#Kj812j3k*(sdkfjasqwercfjeijfdkd"
+    }
+
+    fun resolveToken(request: ServerHttpRequest): String? {
+        val bearerToken = request.headers[TOKEN_HEADER]?.get(0)
+        if (bearerToken.isNullOrEmpty() ||
+            bearerToken.length < TOKEN_PREFIX.length
+        ) return null
+        return bearerToken.substring(TOKEN_PREFIX.length + 1)
+    }
+
+    fun authentication(token: String): Authentication {
+        return UsernamePasswordAuthenticationToken.authenticated(
+            User.withUsername(getUserIdByToken(token))
+                .password("")
+                .authorities(emptyList())
+                .build(), token, emptyList()
+        )
+    }
 
     fun getAccessToken(userId: String): String {
         return createToken(userId, "access")
@@ -21,7 +50,7 @@ class JwtTokenProvider {
             .setSubject(userId)
             .claim("tokenType", tokenType)
             // TODO SecretKey
-            .signWith(SignatureAlgorithm.HS256, "secretKey".toByteArray())
+            .signWith(getSecretKey())
             .compact()
     }
 
@@ -29,11 +58,14 @@ class JwtTokenProvider {
         return getClaimByToken(token).subject
     }
 
-    fun getClaimByToken(token: String): Claims {
-        return Jwts.parser()
-            .setSigningKey("secretKey".toByteArray())
+    private fun getClaimByToken(token: String): Claims {
+        return Jwts.parserBuilder()
+            .setSigningKey(getSecretKey())
+            .build()
             .parseClaimsJws(token)
             .body
     }
+
+    private fun getSecretKey() = Keys.hmacShaKeyFor(SECRET_KEY.toByteArray(StandardCharsets.UTF_8))
 
 }
