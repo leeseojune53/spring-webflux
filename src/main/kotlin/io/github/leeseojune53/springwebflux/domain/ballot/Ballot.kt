b03f6c21944a0b6db3e976fe1701c2e4d59436dc
Leeseojune <sung07288346@gmail.com>
2023-07-06 23:25:10 +0900

    Ballot 추가

---
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/ballot/Ballot.kt
@@ -0,0 +1,3 @@
+package io.github.leeseojune53.springwebflux.domain.ballot
+
+class Ballot
---
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/ballot/service/BallotService.kt
@@ -0,0 +1,20 @@
+package io.github.leeseojune53.springwebflux.domain.ballot.service
+
+class BallotService {
+
+    fun postBallot() {
+        TODO()
+    }
+
+    fun changeBallot() {
+        TODO()
+    }
+
+    fun deleteBallot() {
+        TODO()
+    }
+
+    fun isBalloted(): Boolean {
+        TODO()
+    }
+}
