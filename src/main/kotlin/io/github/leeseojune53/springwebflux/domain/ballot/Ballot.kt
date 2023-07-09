89e1959375f66f160bdfe114240e048817d249d5
Leeseojune <sung07288346@gmail.com>
2023-07-09 23:25:10 +0900

    Ballot, Ballot Entity

--- src/main/kotlin/io/github/leeseojune53/springwebflux/domain/ballot/Ballot.kt
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/domain/ballot/Ballot.kt
@@ -1,3 +1,7 @@
 package io.github.leeseojune53.springwebflux.domain.ballot
 
-class Ballot
+class Ballot(
+    val id: String,
+    val voteId: String,
+    val target: String
+)
---
+++ src/main/kotlin/io/github/leeseojune53/springwebflux/persistence/entity/BallotEntity.kt
@@ -0,0 +1,39 @@
+package io.github.leeseojune53.springwebflux.persistence.entity
+
+import io.github.leeseojune53.springwebflux.domain.ballot.Ballot
+import org.springframework.data.annotation.Id
+import org.springframework.data.domain.Persistable
+import org.springframework.data.relational.core.mapping.Table
+import java.util.UUID
+
+@Table("ballot")
+class BallotEntity(
+    @Id
+    var id: String?,
+    val voteId: String,
+    val target: String
+): Persistable<String> {
+
+    fun toDomain(): Ballot {
+        return Ballot(
+            id = id!!,
+            voteId = voteId,
+            target = target
+        )
+    }
+
+    override fun getId(): String? {
+        return id
+    }
+
+    override fun isNew(): Boolean {
+        if(id == null) {
+            id = UUID.randomUUID().toString()
+            return true
+        }
+        return false
+    }
+
+
+
+}
